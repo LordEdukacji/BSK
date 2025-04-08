a94429d05552aa7841a73aa7ebe9ffcd981c57f2
LordEdukacji <109791175+LordEdukacji@users.noreply.github.com>
2025-04-08 21:22:48 +0200

    Main menu buttons

--- mainApp/src/main/java/pl/edu/pg/student/s193483/Main.java
+++ mainApp/src/main/java/pl/edu/pg/student/s193483/Main.java
@@ -1,7 +1,21 @@
 package pl.edu.pg.student.s193483;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        JFrame frame = new JFrame("Main app");
+        JPanel mainPanel = new JPanel();
+
+        JButton signButton = new JButton("Sign a PDF");
+        JButton verifyButton = new JButton("Verify a signature");
+
+        mainPanel.add(signButton);
+        mainPanel.add(verifyButton);
+
+        frame.add(mainPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
