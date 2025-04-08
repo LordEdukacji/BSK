01b7aeb00914ad0ea31e631c955cd1dbbfb37116
LordEdukacji <109791175+LordEdukacji@users.noreply.github.com>
2025-04-08 23:09:49 +0200

    Temporary file chooser

--- mainApp/src/main/java/pl/edu/pg/student/s193483/Main.java
+++ mainApp/src/main/java/pl/edu/pg/student/s193483/Main.java
@@ -1,6 +1,18 @@
 package pl.edu.pg.student.s193483;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,13 +21,123 @@ public class Main {
 
         JButton signButton = new JButton("Sign a PDF");
         JButton verifyButton = new JButton("Verify a signature");
+        signButton.setEnabled(false);
+        verifyButton.setEnabled(false);
+
+        // move functionality somewhere else later
+        JButton choosePublicButton = new JButton("(temporary) Choose file with public key");
+        JButton choosePrivateButton = new JButton("(temporary) Choose file with private key");
+
+        choosePublicButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                FileNameExtensionFilter fileFilter = new FileNameExtensionFilter("Text Files", "txt");
+                fileChooser.addChoosableFileFilter(fileFilter);
+                fileChooser.setAcceptAllFileFilterUsed(false);
+
+                int openReturn = fileChooser.showOpenDialog(null);
+
+                if (openReturn == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                    String fullPath = file.getAbsolutePath();
+
+                    try (FileInputStream fileInputStream = new FileInputStream(fullPath)) {
+                        byte[] bytes = fileInputStream.readAllBytes();
+                        PublicKey publicKey = KeyExtractor.extractPublicKey(bytes);
+
+                        System.out.println(publicKey);
+                    } catch (FileNotFoundException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (NoSuchAlgorithmException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (InvalidKeySpecException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+
+        choosePrivateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                FileNameExtensionFilter fileFilter = new FileNameExtensionFilter("Text Files", "txt");
+                fileChooser.addChoosableFileFilter(fileFilter);
+                fileChooser.setAcceptAllFileFilterUsed(false);
+
+                int openReturn = fileChooser.showOpenDialog(null);
+
+                if (openReturn == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                    String fullPath = file.getAbsolutePath();
+
+                    try (FileInputStream fileInputStream = new FileInputStream(fullPath)) {
+                        System.out.println("PASSWORD:");
+                        Scanner scanner = new Scanner(System.in);
+                        String password = scanner.nextLine();
+
+                        byte[] bytes = fileInputStream.readAllBytes();
+                        PrivateKey privateKey = KeyExtractor.extractPrivateKey(bytes, password);
+
+                        System.out.println(privateKey);
+                    } catch (FileNotFoundException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (NoSuchAlgorithmException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (InvalidKeySpecException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (InvalidAlgorithmParameterException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (NoSuchPaddingException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (IllegalBlockSizeException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (BadPaddingException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (InvalidKeyException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
 
         mainPanel.add(signButton);
         mainPanel.add(verifyButton);
+
+        // remove later
+        mainPanel.add(choosePublicButton);
+        mainPanel.add(choosePrivateButton);
 
         frame.add(mainPanel);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        try {
+            byte[] b = Files.readAllBytes(Path.of("C:/Users/ebede/Desktop/priv.txt"));
+
+            PrivateKey pk = KeyExtractor.extractPrivateKey(b, "abc");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidAlgorithmParameterException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
